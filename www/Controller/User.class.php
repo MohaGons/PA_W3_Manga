1fd4ef0a2931c5ada894e7179540545687a1df21
Maxime-Lao <Maxime.laotonlop@gmail.com>
2022-02-04 15:33:57 +0100

    Add register page

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -26,24 +26,25 @@ class User {
     {
         $user = new UserModel();
 
-        if(!empty($_POST)) {
+        if (isset($_POST['submit'])){
+            $errors = Verificator::checkForm($user->getRegisterForm(), $_POST);
+            if(!empty($_POST)) {
+                if(empty($errors)){
 
-            $result = Verificator::checkForm($user->getRegisterForm(), $_POST);
-            print_r($result);
+					$user->setFirstname(htmlspecialchars($_POST["firstname"]));
+					$user->setLastname(htmlspecialchars($_POST["lastname"]));
+					$user->setEmail(htmlspecialchars($_POST["email"]));
+					$user->setPassword(password_hash(htmlspecialchars($_POST["password"]), PASSWORD_BCRYPT));
+                    $user->setGender(htmlspecialchars($_POST["gender"]));
+					$user->setAvatar(password_hash(htmlspecialchars($_POST["avatar"]), PASSWORD_BCRYPT));
 
-            if (empty($result)) {
-                $user->setFirstname(htmlspecialchars($_POST["firstname"]));
-                $user->setLastname(htmlspecialchars($_POST["lastname"]));
-                $user->setEmail(htmlspecialchars($_POST["email"]));
-                $user->setPassword(password_hash(htmlspecialchars($_POST["password"]), PASSWORD_BCRYPT));
-                $user->setGender(htmlspecialchars($_POST["gender"]));
-                $user->setAvatar(password_hash(htmlspecialchars($_POST["avatar"]), PASSWORD_BCRYPT));
-
-                $user->save();
-                echo "<script>alert('Votre profil a bien été mis à jour')</script>";
+					$user->save();
+					echo "<script>alert('Votre profil a bien été mis à jour')</script>";
+				} else{
+					print_r($errors);
+				}
             }
         }
-        
 
 
         $view = new View("Register");
