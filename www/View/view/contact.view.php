3c414581e03d1a75aa62f019839c319ee17f0a2f
Nfassory Diaby <fassory.diaby@gmail.com>
2022-02-26 15:19:41 +0100

    add components button

--- www/View/view/contact.view.php
+++ www/View/view/contact.view.php
@@ -1,3 +1,11 @@
 <form>
     <input>
+    <button class="button button--primary">Contact</button>
+    <button class="button button--secondary">Contact</button>
+    <button class="button button--success">Contact</button>
+    <button class="button button--danger">Contact</button>
+    <button class="button button--warning">Contact</button>
+    <button class="button button--info">Contact</button>
+    <button class="button button--light">Contact</button>
+    <button class="button button--dark">Contact</button>
 </form>
