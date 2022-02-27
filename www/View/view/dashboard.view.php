e9b4a3c917c1954d13074719a59bc068de580585
Maxime-Lao <Maxime.laotonlop@gmail.com>
2022-02-27 16:08:28 +0100

    Add chart in card

--- www/View/view/dashboard.view.php
+++ www/View/view/dashboard.view.php
@@ -1,4 +1,26 @@
 <h1>Tableau de bord</h1>
 Welcome <?= $firstname?> <?= $lastname ?>
+<div class="col-lg-5">
+<div class="card card--lg">
+    <canvas id="myChart"></canvas>
+</div>
+</div>
 
+<div class="col-lg-4">
+<div class="card">
+        <h4>Activités</h4>
+        <div class="card-content">
+            <div class="text-card">
+                <h2>02</h2>
+                <p>Visiteur unique sur votre site</p>
+            </div>
+            <div id="borderline"></div>
+            <div class="text-card">
+                <h2>50</h2>
+                <p>Apparitions dans les recherches internet</p>
+            </div>
+        </div>
+    </div>
+
+</div>
 
