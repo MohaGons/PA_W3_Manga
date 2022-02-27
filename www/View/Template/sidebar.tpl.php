42397667194d1e70b323d131bc37203aca81fa6f
Nfassory Diaby <fassory.diaby@gmail.com>
2022-02-27 18:07:02 +0100

    add integration dashboard admin

---
+++ www/View/Template/sidebar.tpl.php
@@ -0,0 +1,14 @@
+<div id="headerSidebar" class="sidebar">
+    <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Template</a></li>
+        <li><a href="#">Pages</a></li>
+        <li><a href="#">Animé/manga</a></li>
+        <li><a href="#">Evenements</a></li>
+        <li><a href="#">Commentaires</a></li>
+        <li><a href="#">Utilisateurs</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Catégories</a></li>
+        <li><a href="#">Parametres</a></li>
+    </ul>
+</div>
