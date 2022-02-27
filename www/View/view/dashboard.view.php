c45c514fa41ab881cd2fa1cd70d9ba254aa2b866
Nfassory Diaby <fassory.diaby@gmail.com>
2022-02-27 18:07:02 +0100

    add integration dashboard admin

--- www/View/view/dashboard.view.php
+++ www/View/view/dashboard.view.php
@@ -1,4 +1,28 @@
-<h1>Tableau de bord</h1>
-Welcome <?= $firstname?> <?= $lastname ?>
+<div class="container">
+    <h1>Dashboard</h1>
+    <h2>Nom : MonSitedeouf</h2>
+    Welcome <?= $firstname?> <?= $lastname ?>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-12 col-xl-5">
+            <div class="card"></div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-12 col-xl-5">
+            <div class="card"></div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-8">
+            <div class="card"></div>
+
+        </div>
+
+
+    </div>
+
+</div>
+
 
 
