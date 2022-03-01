f3056ad852087f13410ddc7e4336381963b69136
Nfassory Diaby <fassory.diaby@gmail.com>
2022-03-01 16:57:31 +0100

    add dashboard view admin

--- www/View/Template/sidebar.tpl.php
+++ www/View/Template/sidebar.tpl.php
@@ -1,14 +1,14 @@
 <div id="headerSidebar" class="sidebar">
     <ul>
-        <li><a href="#">Dashboard</a></li>
-        <li><a href="#">Template</a></li>
-        <li><a href="#">Pages</a></li>
-        <li><a href="#">Animé/manga</a></li>
-        <li><a href="#">Evenements</a></li>
-        <li><a href="#">Commentaires</a></li>
-        <li><a href="#">Utilisateurs</a></li>
-        <li><a href="#">Articles</a></li>
-        <li><a href="#">Catégories</a></li>
-        <li><a href="#">Parametres</a></li>
+        <li><a href="#"><i class="fa-solid fa-house"></i> Dashboard</a></li>
+        <li><a href="#"><i class="fa-solid fa-file"></i> Template</a></li>
+        <li><a href="#"><i class="fa-solid fa-file"></i> Pages</a></li>
+        <li><a href="#"><i class="fa-brands fa-napster"></i> Animé/manga</a></li>
+        <li><a href="#"><i class="fa-solid fa-champagne-glasses"></i> Evenements</a></li>
+        <li><a href="#"><i class="fa-solid fa-comment-dots"></i> Commentaires</a></li>
+        <li><a href="#"><i class="fa-solid fa-user"></i> Utilisateurs</a></li>
+        <li><a href="#"><i class="fa-solid fa-newspaper"></i> Articles</a></li>
+        <li><a href="#"><i class="fa-solid fa-calendar"></i> Catégories</a></li>
+        <li><a href="#"><i class="fa-solid fa-gear"></i> Parametres</a></li>
     </ul>
 </div>
