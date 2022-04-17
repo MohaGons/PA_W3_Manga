1e6aae3a8dd2ff7b9045a4a76d6d7e4c96aa8dde
Nfassory Diaby <fassory.diaby@gmail.com>
2022-04-17 17:14:25 +0200

    update mdp mailer

--- www/Core/Mailer.class.php
+++ www/Core/Mailer.class.php
@@ -23,7 +23,7 @@ class Mailer
             $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'mangasite2022@gmail.com';              //SMTP username
-            $mail->Password   = 'Mangasite!2022';                       //SMTP password
+            $mail->Password   = 'namvkbbhnlpokjqr';                       //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
