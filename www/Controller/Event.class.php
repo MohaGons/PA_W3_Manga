4747618c1d5533ba91a26c2501d401ba805b86e8
levi <levimalakias@gmail.com>
2022-07-10 14:55:09 +0200

    Modif controller event

--- www/Controller/Event.class.php
+++ www/Controller/Event.class.php
@@ -38,4 +38,41 @@ class Event
         $view->assign("event_data", $event);
         $view->assign("errors", $errors);
     }
+
+    public function editEvent()
+    {
+        $event = new EventModel();
+        $view = new View("edit-event", "back");
+        $view->assign("event", $event);
+        $errors = [];
+        $event_data = $event->getEvents($_GET["id"]);
+
+        if (!empty($_POST)) {
+            $result = Verificator::checkFormParam($event->getEventEditFormRegister($event_data), $_POST);
+
+            if (empty($result)) {
+                $event->setId($_GET["id"]);
+                if (!empty($_POST["editName"])) {
+                    $event->setName(htmlspecialchars($_POST["editName"]));
+                }
+                if (!empty($_POST["editDescription"])) {
+                    $event->setDescription(htmlspecialchars($_POST["editDescription"]));
+                }
+                if (!empty($_POST["editDate"])) {
+                    $event->setDate(htmlspecialchars($_POST["editDate"]));
+                }
+                if (!empty($_POST["editPrice"])) {
+                    $event->setPrice(htmlspecialchars($_POST["editPrice"]));
+                }
+                if (!empty($_POST["editPhoto"])) {
+                    $event->setPhoto(htmlspecialchars($_POST["editPhoto"]));
+                }
+                $event->save();
+                //echo "<script>alert('Votre catégorie a bien été mis à jour')</script>";
+                header('Location: ./event');
+            } else {
+                $errors = $result;
+            }
+        }
+    }
 }
