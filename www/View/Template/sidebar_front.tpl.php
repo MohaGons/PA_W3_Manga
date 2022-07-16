e06b396d726b476d8784d6e3914b4bd78aad01cd
Amine <aminecheriguifr@gmail.com>
2022-07-16 19:37:33 +0200

    update events

--- www/View/Template/sidebar_front.tpl.php
+++ www/View/Template/sidebar_front.tpl.php
@@ -3,4 +3,5 @@
         <li><a href="/">Accueil</a></li>
         <!-- <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li> -->
                 <li><a href="/page-1">page 1</a></li>
+                <li><a href="/evenement-test-page">evenement test page</a></li>
         
