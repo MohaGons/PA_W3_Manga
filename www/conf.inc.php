4cd7b5f9b6dc58b31c6b30401f8edea0365e4e42
Nfassory Diaby <fassory.diaby@gmail.com>
2022-08-22 15:23:41 +0200

    add conf

---
+++ www/conf.inc.php
@@ -0,0 +1,17 @@
+
+<?php
+define("WEBSITE_NAME","mangasite.com");
+define("WEBSITE_CONTACT_ADDRESS","mangasite2022@gmail.com");
+define("ENV","prod");
+define("DB_HOST","database");
+define("DB_DRIVER","mysql");
+define("DB_PORT","3306");
+define("DB_NAME","pa_database");
+define("DB_PREFIXE","mnga_");
+define("DB_USER","root");
+define("DB_PASSWORD","password");
+define("EMAIL_SOURCE_NAME","no-reply@mangasite.fr");
+define("EMAIL_SMTP_HOST","smtp.gmail.com");
+define("EMAIL_SMTP_ADMIN","mangasite2022@gmail.com");
+define("EMAIL_SMTP_PASSWORD","namvkbbhnlpokjqr");
+define("EMAIL_SMTP_PORT","465");
