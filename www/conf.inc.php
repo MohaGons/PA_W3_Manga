600f57c4ce1d16fd7cf953e964d189dd520aa386
Nfassory Diaby <fassory.diaby@gmail.com>
2022-08-25 20:22:01 +0200

    fix mailer

--- www/conf.inc.php
+++ www/conf.inc.php
@@ -10,6 +10,16 @@ define("DB_NAME", "pa_database");
 define("DB_PORT", "3306");
 define("DB_PREFIXE", "mnga_");
 
+define("WEBSITE_NAME","mangasite.com");
+define("WEBSITE_CONTACT_ADDRESS","mangasite2022@gmail.com");
+define("ENV","prod");
+
+define("EMAIL_SOURCE_NAME","no-reply@mangasite.fr");
+define("EMAIL_SMTP_HOST","smtp.gmail.com");
+define("EMAIL_SMTP_ADMIN","mangasite2022@gmail.com");
+define("EMAIL_SMTP_PASSWORD","namvkbbhnlpokjqr");
+define("EMAIL_SMTP_PORT","465");
+
 //LIEN VIEW
 define("ADMIN_HOME_ROUTE", "/admin");
 define("HOME_ROUTE", "/");
